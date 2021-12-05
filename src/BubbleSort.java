0f28d0f41178015d67d5c0bf687888828b1f371a
popcornto <jazmati6@gmail.com>
2021-12-05 01:14:19 +0100

    BubbuSorte

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,12 +1,15 @@
 public class BubbleSort {
 
+    private static Object ArithmeticException;
+
     public static void main(String[] args) {
         int[] arr = {4, 9, 3, 5, 2, 0, 1, 8};
+        int[] arr1 = {};
         System.out.println(isSortedrek(arr, 7));
         System.out.println(isSorted(arr));
-        for (int elemnt : BubbaSorst(arr, 7)
+        for (int elemnt : BubbaSorst(arr, 0)
         ) {
-            System.out.println(elemnt);
+            System.out.print(elemnt);
         }
     }
 
@@ -34,7 +37,11 @@ public class BubbleSort {
     }
 
     public static int[] BubbaSorst(int[] arr, int i) {
+        if (arr.length == 0){
+            return null;
+        }
         int full = arr.length;
+        if (arr.length == 1){return arr;}
         if (i >= 1) {
             int swapfirst = arr[i];
             int swapsecond = arr[i - 1];
